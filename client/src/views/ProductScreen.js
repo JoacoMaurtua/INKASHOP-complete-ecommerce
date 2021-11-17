1751e0d4cffc4d89221bab6fb1dab6307a292420
JoacoMaurtua <joacomaurtua@gmail.com>
2021-11-17 15:12:24 -0500

    estructura del producto

--- client/src/views/ProductScreen.js
+++ client/src/views/ProductScreen.js
@@ -1,10 +1,54 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+import {Row,Col,Image,ListGroup,Card,Button, ListGroupItem} from 'react-bootstrap';
+import Rating from '../components/Rating';
+import products from '../products';
+ 
+const Productscreen = ({match}) => {
 
-const Productscreen = () => {
+  const product = products.find((p) => p._id === match.params.id)
+
   return (
-    <div>
-      hello
-    </div>
+    <>
+      <Link className="btn btn-dark my-3" to='/'>
+        Go Back
+      </Link>
+      <Row>
+        <Col md={6}>
+          <Image src={product.image} alt={product.name} fluid/>
+        </Col>
+        <Col md={3}>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <h3>{product.name}</h3>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Rating value={product.rating} text={`${product.numReviews} reviews`}/>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <strong>Price: ${product.price}</strong>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              Description: {product.description}
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col md={3}>
+          <Card>
+            <ListGroup variant='flush'>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Price:</Col>
+                  <Col>
+                    <strong>${product.price}</strong>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
+    </>
   );
 }
 
