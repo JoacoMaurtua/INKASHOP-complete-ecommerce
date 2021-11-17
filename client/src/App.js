20f3e0c538a1f8f1b9a95716eb2820e56f15f569
JoacoMaurtua <joacomaurtua@gmail.com>
2021-11-17 10:36:42 -0500

    lista de productos mapeados

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,15 @@ import {Container} from 'react-bootstrap';
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import HomeScreen from './views/HomeScreen';
 
 function App() {
   return (
     <>
       <Header/>
-      <main className="py-3">
+      <main className="gy-3">
         <Container>
-          <h1>Welcome to InkaShop</h1>
+          <HomeScreen/>
         </Container>
       </main>
       <Footer/>
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -9,7 +9,7 @@ const Header = () => {
           <Navbar.Brand href='/'>InkaShop</Navbar.Brand>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
-            <Nav className='ml-auto' style={{marginLeft: '60rem'}}>
+            <Nav className='ms-auto'>
               <Nav.Link href='/cart'>
                 <i className='fas fa-shopping-cart'></i> Cart
               </Nav.Link>
