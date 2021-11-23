44438e55543e65c1160a3eccc0dbb9b14c7a5736
JoacoMaurtua <joacomaurtua@gmail.com>
2021-11-23 14:20:22 -0500

    creando el store de redux

---
+++ client/src/store.js
@@ -0,0 +1,25 @@
+import {createStore, combineReducers, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
+import {composeWithDevTools} from 'redux-devtools-extension';
+
+//variable que engloba a los reducers(funciones) y combina sus propiedades en un solo objeto
+const reducer = combineReducers({
+  
+});
+
+const initialState = {};
+
+const middleware = [thunk] // middlerarw para hacer llamadas asincronas
+
+const store = createStore(reducer,initialState,composeWithDevTools(
+  applyMiddleware(...middleware)
+));
+
+export default store;
+
+
+
+
+
+
+
