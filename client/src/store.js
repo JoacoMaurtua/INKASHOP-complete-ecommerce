9448e87c182ddb83c8efcdee857266e7d73a1115
JoacoMaurtua <joacomaurtua@gmail.com>
2021-11-23 16:57:59 -0500

    productReducers y productActions creados

--- client/src/store.js
+++ client/src/store.js
@@ -1,10 +1,10 @@
 import {createStore, combineReducers, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
 import {composeWithDevTools} from 'redux-devtools-extension';
-
+import {productListReducer} from './reducers/productReducers';
 //variable que engloba a los reducers(funciones) y combina sus propiedades en un solo objeto
 const reducer = combineReducers({
-  
+  productList: productListReducer,
 });
 
 const initialState = {};
