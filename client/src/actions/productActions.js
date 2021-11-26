4199a5952936215dc423e6270cd28d0db3e7b655
JoacoMaurtua <joacomaurtua@gmail.com>
2021-11-25 22:44:58 -0500

    flujo de datos redux para cada producto en particular

--- client/src/actions/productActions.js
+++ client/src/actions/productActions.js
@@ -1,4 +1,6 @@
 import {PRODUCT_LIST_REQUEST,PRODUCT_LIST_SUCCESS,PRODUCT_LIST_FAIL} from '../constants/productConstants';
+import {PRODUCT_DETAILS_REQUEST,PRODUCT_DETAILS_SUCCESS,PRODUCT_DETAILS_FAIL} from '../constants/productConstants';
+
 import axios from 'axios';
 
 //Funcion asincrona creadora de acciones
@@ -16,6 +18,28 @@ export const listProducts = () => async(dispatch) =>{
   }catch(error){
     dispatch({
       type:PRODUCT_LIST_FAIL,
+      payload: error.response && error.response.data.message? //mismo error que apareceria en mongoDB atlas
+      error.response.data.message
+      :error.message
+    })
+  }
+}
+
+
+export const listDetailsProduct = (id) => async(dispatch) =>{
+  try{
+    dispatch({type: PRODUCT_DETAILS_REQUEST}) //dispatch() envia la accion al store
+
+    const {data} = await axios.get(`/api/product/${id}`); //se extrae los datos de la API productos
+
+    dispatch({
+      type: PRODUCT_DETAILS_SUCCESS, //tipo de accion a realizar
+      payload: data //array que contiene todos los objetos producto en la base de datos
+    })
+
+  }catch(error){
+    dispatch({
+      type:PRODUCT_DETAILS_FAIL,
       payload: error.response && error.response.data.message? //mismo error que apareceria en mongoDB atlas
       error.response.data.message
       :error.message
--- client/src/store.js
+++ client/src/store.js
@@ -1,10 +1,11 @@
 import {createStore, combineReducers, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
 import {composeWithDevTools} from 'redux-devtools-extension'; //metodo para que las redux dev tools del browser se apliquen al proyecto
-import {productListReducer} from './reducers/productReducers';
+import {productListReducer, productListDetailsReducer} from './reducers/productReducers';
 //Variable que engloba a los reducers y combina sus propiedades en un solo objeto
 const reducer = combineReducers({
-  productList: productListReducer
+  productList: productListReducer,
+  productDetails: productListDetailsReducer
 });
 
 const initialState = {}; //estado inicial de la aplicacion(recibira los datos de la API)
