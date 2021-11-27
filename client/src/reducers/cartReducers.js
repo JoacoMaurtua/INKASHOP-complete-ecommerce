27c54b7c051961cf8cabb60365a76cce0745073f
JoacoMaurtua <joacomaurtua@gmail.com>
2021-11-26 23:10:32 -0500

    creando el action de cart con info en local storage

--- client/src/reducers/cartReducers.js
+++ client/src/reducers/cartReducers.js
@@ -1,6 +1,6 @@
 import { CART_ADD_ITEM } from '../constants/cartConstants';
 
-const cartReducers = (state = { cartItems: [] }, action) => {
+export const cartReducer = (state = { cartItems: [] }, action) => {
   switch (action.type) {
     case CART_ADD_ITEM:
       const item = action.payload;
@@ -26,4 +26,4 @@ const cartReducers = (state = { cartItems: [] }, action) => {
   }
 };
 
-export default cartReducers;
+
--- client/src/store.js
+++ client/src/store.js
@@ -2,13 +2,21 @@ import {createStore, combineReducers, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
 import {composeWithDevTools} from 'redux-devtools-extension'; //metodo para que las redux dev tools del browser se apliquen al proyecto
 import {productListReducer, productListDetailsReducer} from './reducers/productReducers';
+import {cartReducer} from './reducers/cartReducers';
+
 //Variable que engloba a los reducers y combina sus propiedades en un solo objeto
 const reducer = combineReducers({
   productList: productListReducer,
-  productDetails: productListDetailsReducer
+  productDetails: productListDetailsReducer,
+  cart: cartReducer
 });
 
-const initialState = {}; //estado inicial de la aplicacion(recibira los datos de la API)
+const cartItemsFromStorage = localStorage.getItem('cartItems')? 
+JSON.parse( localStorage.getItem('cartItems')): []
+
+const initialState = {
+  cart: {cartItems: cartItemsFromStorage}
+}; //estado inicial de la aplicacion(recibira los datos de la API)
 
 const middleware = [thunk] // middleware para hacer llamadas asincronas en el mundo de redux
 
