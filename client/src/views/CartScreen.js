984cedfaf6500806e680b5a02bcbacfb17bc9695
Stefano <piero.quiroz93@gmail.com>
2021-12-07 21:29:44 -0500

    Location search

--- client/src/views/CartScreen.js
+++ client/src/views/CartScreen.js
@@ -1,6 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Link, useParams, useLocation } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { Row, Col, ListGroup, image, Form, Button, Card } from 'react-bootstrap';
+import Message from '../components/Message';
+import { addCart } from '../actions/cartActions';
 
 const Cartscreen = () => {
+  const {id} = useParams();
+
+  const location = useLocation();
+  const qty = location.search;
+  
+  console.log(qty);
+  //useEffect()
   return (
     <div>
       Cart
