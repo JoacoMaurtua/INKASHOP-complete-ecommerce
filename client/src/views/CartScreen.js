431dadb70280032cc58e4bae7285656a830b40c3
JoacoMaurtua <joacomaurtua@gmail.com>
2021-12-07 22:08:15 -0500

    enviando la data del carrito al store

--- client/src/views/CartScreen.js
+++ client/src/views/CartScreen.js
@@ -1,8 +1,29 @@
-import React, {useEffect} from 'react';
-import {useDispatch, useSelector} from 'react-redux';
-
+import React, { useEffect } from 'react';
+import { Link, useParams, useLocation } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { Row, Col, ListGroup, image, Form, Button, Card } from 'react-bootstrap';
+import Message from '../components/Message';
+import { addCart } from '../actions/cartActions';
 
 const Cartscreen = () => {
+  const {id} = useParams();
+
+  const location = useLocation();
+
+  const qty = location.search?Number(location.search.split('=')[1]):1;
+
+  const dispatch = useDispatch();
+
+  const cart = useSelector((state) => state.cart)
+  const {cartItems} = cart;
+  
+  console.log(qty);
+  console.log(location)
+
+  useEffect(()=>{
+      dispatch(addCart(id,qty))
+  },[dispatch,id,qty])
+
   return (
     <div>
       Cart
@@ -11,12 +32,3 @@ const Cartscreen = () => {
 }
 
 export default Cartscreen;
-
-
-
-
-
-//Location es una variable de react-router-dom
-//que trabaja con la URL
-
-//location.search --> Existe esa cantidad?
