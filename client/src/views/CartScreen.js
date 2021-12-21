57df454c3db36d5073e580970c3193eeeeb95d8f
JoacoMaurtua <joacomaurtua@gmail.com>
2021-12-21 12:27:29 -0500

    interfaz de cada producto en el carrito

--- client/src/views/CartScreen.js
+++ client/src/views/CartScreen.js
@@ -5,7 +5,7 @@ import {
   Row,
   Col,
   ListGroup,
-  image,
+  Image,
   Form,
   Button,
   Card,
@@ -29,25 +29,65 @@ const Cartscreen = () => {
   console.log(location);
 
   useEffect(() => {
-    if(id){
+    if (id) {
       dispatch(addCart(id, qty)); //envio estos parametros al actions
     }
   }, [dispatch, id, qty]);
+
+  const removeFromHandler = (id) =>{
+    console.log('remove');
+  }
 
   return (
     <Row>
       <Col md={8}>
         <h1>Shopping Cart</h1>
-        {
-          cartItems.length === 0?(
-            <Message>
-              Your cart is empty <Link to='/'>Go Back</Link>
-            </Message>
-          ):(
-            <ListGroup variant='flush'></ListGroup>
-          ) 
-         
-        }
+        {cartItems.length === 0 ? (
+          <Message>
+            Your cart is empty <Link to="/">Go Back</Link>
+          </Message>
+        ) : (
+          <ListGroup variant="flush">
+            {cartItems.map((item) => (
+              <ListGroup.Item key={item.product}>
+                <Row>
+                  <Col md={2}>
+                    <Image src={item.image} alt={item.name} fluid rounded />
+                  </Col>
+                  <Col md={3}>
+                    <Link to={`product/${item.product}`}>{item.name}</Link>
+                  </Col>
+                  <Col md={2}>{item.price}</Col>
+                  <Col md={2}>
+                    <Form.Select
+                      value={qty}
+                      onChange={(e) =>
+                        dispatch(addCart(item.product, Number(e.target.value)))
+                      }
+                    >
+                      {' '}
+                      {/* cambia el estado*/}
+                      {[...Array(item.countInStock).keys()].map(
+                        (
+                          x /* Crear un arreglo con el numero de posiciones que tanga el stock */
+                        ) => (
+                          <option key={x + 1} value={x + 1}>
+                            {x + 1}
+                          </option>
+                        )
+                      )}
+                    </Form.Select>
+                  </Col>
+                  <Col md={2}>
+                      <Button type='button' variant='light' onClick={()=>removeFromHandler(item.product)}>
+                        <i className="fa fa-trash"></i>
+                      </Button>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+            ))}
+          </ListGroup>
+        )}
       </Col>
 
       <Col md={2}></Col>
