d6dd4be98d4e10153655d6b0b859e955ddda2ed6
JoacoMaurtua <joacomaurtua@gmail.com>
2022-01-14 21:08:07 -0500

    middleware de autenticacion personalizada

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -2,11 +2,15 @@ const express = require('express');
 
 const router = express();
 
-const {authUser} = require('../controllers/user.controllers');
+const {authUser,getUserProfile} = require('../controllers/user.controllers');
+
+const {protect} = require('../middleware/addMiddleware');
 
 //Rutas:
 
 router.post('/login',authUser)
+
+router.get('/profile',getUserProfile,protect)
 
 
 module.exports = router;
