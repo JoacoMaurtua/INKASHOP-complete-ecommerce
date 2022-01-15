23a9db901d6ad7fa8408485b427778596c2c4c4e
JoacoMaurtua <joacomaurtua@gmail.com>
2022-01-15 11:20:15 -0500

    modificacion

--- server/middleware/addMiddleware.js
+++ server/middleware/addMiddleware.js
@@ -6,39 +6,33 @@ const asyncHandler = require('express-async-handler');
 
 //MIDDLEWARE PARA VALIDAR EL TOKEN
 
-const protect = asyncHandler(async(req, res, next)=>{
-  let token 
+const protect = asyncHandler(async (req, res, next) => {
+  let token
 
-  if(req.headers.authorization){
-    
-    try{
+  if (
+    req.headers.authorization 
+  ) {
+    try {
       token = req.headers.authorization
-
-      console.log('token: ',token)
 
       const decoded = jwt.verify(token, process.env.JWT_SECRET)
 
-      req.user = await User.findById(decoded.id).select('-password') //buscar por el ID y selecciona el password de esa data
+      req.user = await User.findById(decoded.id).select('-password')
 
-      console.log(decoded)
-
-      next();
-
-    }catch(error){
+      next()
+    } catch (error) { //AQUI CAE EL ERROR
       console.error(error)
       res.status(401)
       throw new Error('Not authorized, token failed')
     }
   }
 
-  if(!token){
+  if (!token) {
     res.status(401)
     throw new Error('Not authorized, no token')
   }
-
-  console.log(req.header.authorization)
-
-  next();
 })
+
+
 
 module.exports = {protect};
