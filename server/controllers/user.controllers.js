ce32e1890861273d36e454185203e5661d731812
JoacoMaurtua <joacomaurtua@gmail.com>
2022-01-15 12:08:15 -0500

    funcion registerUser en controllers

--- server/controllers/user.controllers.js
+++ server/controllers/user.controllers.js
@@ -2,6 +2,7 @@ const User = require('../models/user.models');
 const asyncHandler = require('express-async-handler');
 const generateToken = require('../utils/generateToken');
 
+//LOGIN
 const authUser = asyncHandler(async (req, res) => {
   const {email,password} = req.body
 
@@ -24,6 +25,38 @@ const authUser = asyncHandler(async (req, res) => {
   }
 }) 
 
+//REGISTER
+const registerUser = asyncHandler(async (req, res) => {
+  const {name,email,password} = req.body
+
+  const userExists = await User.findOne({email})
+
+  if(userExists){
+    res.status(400)
+    throw new Error('User already exists')
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password //plain text por ahora
+  })
+
+  if(user){
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id)
+    })
+  }else{
+      res.status(400)
+      throw new Error('Invalid user data')
+  }
+  
+}) 
+
 const getUserProfile = (req, res) => {
   //res.send("Success")
   User.findById(req.user._id)
@@ -40,4 +73,4 @@ const getUserProfile = (req, res) => {
 }
 
 
-module.exports = {authUser, getUserProfile}
+module.exports = {authUser, getUserProfile,registerUser}
