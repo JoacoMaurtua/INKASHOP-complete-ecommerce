a818a6b0da5eea295fe25d294a67174f7ade098d
JoacoMaurtua <joacomaurtua@gmail.com>
2022-01-17 10:39:43 -0500

    userReducer

---
+++ client/src/reducers/userReducers.js
@@ -0,0 +1,25 @@
+import {
+  USER_LOGIN_REQUEST,
+  USER_LOGIN_SUCCESS,
+  USER_LOGIN_FAIL,
+  USER_LOGOUT,
+} from '../constants/userConstants';
+
+export const userLoginReducer = (state = {}, action) => {
+  switch (action.type) {
+    case USER_LOGIN_REQUEST:
+      return { loading: true }; //retorna un objeto nuevo con el estado(array) aun vacio
+
+    case USER_LOGIN_SUCCESS:
+      return { loading: false, userInfo: action.payload }; //retorna un objeto nuevo con el estado(array) lleno con los datos traidos por axios de la DB
+
+    case USER_LOGIN_FAIL:
+      return { loading: false, error: action.payload }; //retorna un objeto nuevo que contiene el error definido en productActions.js
+
+    case USER_LOGOUT:
+      return {};
+
+    default:
+      return state; //retorna el mismo estado en caso no se envie una accion valida
+  }
+};
