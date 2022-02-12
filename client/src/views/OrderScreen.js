3725ff404c4307408e7cc0a7407d964505b77c89
JoacoMaurtua <joacomaurtua@gmail.com>
2022-02-11 22:02:43 -0500

    terminado order screen con alertas

--- client/src/views/OrderScreen.js
+++ client/src/views/OrderScreen.js
@@ -33,12 +33,12 @@ const OrderScreen = () => {
   console.log({order:order})
 
   useEffect(() =>{
-   dispatch(getOrderDetails(id))
-  },[dispatch,id])
+    if(!order || order._id !== id) {
+      dispatch(getOrderDetails(id))
+  }
+  },[dispatch,id,order])
 
  
-
-
 
   return loading ? <Loader/> : error ? <Message variant='danger'>{error}</Message>:<>
     <h1>Order {order._id}</h1>
@@ -47,6 +47,9 @@ const OrderScreen = () => {
           <ListGroup variant="flush">
             <ListGroup.Item>
               <h2>Shipping</h2>
+              <p><strong>Name: </strong>{order.user.name}</p>
+              <p><strong>Email: </strong><a href={`mailto${order.user.email}`}>{order.user.email}</a></p>
+
               <p>
                 <strong>Address: </strong>
                 {order.shippingAddress.address}
@@ -54,12 +57,24 @@ const OrderScreen = () => {
                 {order.shippingAddress.postalCode}
                 {order.shippingAddress.country}
               </p>
+              {order.idDelivered?(
+                <Message variant='success'>Delivered on {order.idDelivered}</Message>
+              ):(
+                <Message variant='danger'>Not Delivered</Message>
+              )}
             </ListGroup.Item>
 
             <ListGroup.Item>
               <h2>Payment Method</h2>
-              <strong>Method:</strong>
-              {order.paymentMethod}
+              <p>
+                <strong>Method: </strong>
+                {order.paymentMethod}
+              </p>
+              {order.isPaid?(
+                <Message variant='success'>Paid on {order.paidAt}</Message>
+              ):(
+                <Message variant='danger'>Not Paid</Message>
+              )}
             </ListGroup.Item>
 
             <ListGroup.Item>
