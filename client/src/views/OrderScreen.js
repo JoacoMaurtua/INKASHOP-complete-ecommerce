3706646bc7f3210dafa4f12ad92089727b208592
JoacoMaurtua <joacomaurtua@gmail.com>
2022-02-16 15:40:17 -0500

    error con order.paidAt y order.isPaid

--- client/src/views/OrderScreen.js
+++ client/src/views/OrderScreen.js
@@ -5,79 +5,100 @@ import { useParams } from 'react-router-dom';
 import Loader from '../components/Loader';
 import Message from '../components/Message';
 import { Link } from 'react-router-dom';
-import {getOrderDetails} from '../actions/orderActions';
+import { getOrderDetails, payOrder } from '../actions/orderActions';
 import axios from 'axios';
+import { PayPalButton } from 'react-paypal-button-v2';
+import { ORDER_PAY_RESET } from '../constants/orderConstants';
 
 const OrderScreen = () => {
   const dispatch = useDispatch();
 
-  const {id} = useParams();
+  const { id } = useParams();
 
   //Paquete SDK de payPal
   const [sdkReady, setSdkReady] = useState(false);
-  console.log({sdkReady:sdkReady});
-  
+  console.log({ sdkReady: sdkReady });
+
   //necesitamos envolver toda los datos de la orden en un estado
 
-  const orderDetails = useSelector(state => state.orderDetails)
-  const {order, loading, error} = orderDetails;
+  const orderDetails = useSelector((state) => state.orderDetails);
+  const { order, loading, error } = orderDetails;
 
   //comprobar si el pago se ha hecho correctamente
-  const orderPay = useSelector(state => state.orderPay)
-  const {loading:loadingPay, success:successPay} = orderPay;
-
-
+  const orderPay = useSelector((state) => state.orderPay);
+  const { loading: loadingPay, success: successPay } = orderPay;
 
   if (!loading) {
     const addDecimals = (num) => {
-      return (Math.round(num * 100) / 100).toFixed(2)
-    }
+      return (Math.round(num * 100) / 100).toFixed(2);
+    };
 
     order.itemsPrice = addDecimals(
       order.orderItems.reduce((acc, item) => acc + item.price * item.qty, 0)
     )
   }
 
-  console.log({order:order})
+  console.log({ order: order });
 
-  useEffect(() =>{
-
+  useEffect(() => {
     //Funcion asyncrona para agregar el script del sdk de paypal al codigo fuente HTML
-    const addPayPalScript = async() =>{
-      const {data: clientId} = await axios.get('/api/config/paypal')
-      const script = document.createElement('script')
-      script.type = 'text/javascript'
-      script.src = `https://www.paypal.com/sdk/js?client-id=${clientId}`
-      script.async = true
-      script.onload = () =>{
-        setSdkReady(true)
+    const addPayPalScript = async () => {
+      const { data: clientId } = await axios.get('/api/config/paypal');
+      const script = document.createElement('script');
+      script.type = 'text/javascript';
+      script.src = `https://www.paypal.com/sdk/js?client-id=${clientId}`;
+      script.async = true;
+      script.onload = () => {
+        setSdkReady(true);
+      };
+      document.body.appendChild(script);
+    };
+
+    
+    
+
+    if (!order || order._id !== id || successPay) {
+      dispatch({type: ORDER_PAY_RESET}) //sin esto, una vez que se hace el pago, la pagina se resguira refrescando
+      dispatch(getOrderDetails(id));
+    } else if (!order.isPaid) {
+      //Si no se ha pagado la orden, no muestra la interfaz de payPal
+      if (!window.paypal) {
+        addPayPalScript();
+      } else {
+        setSdkReady(true);
       }
-      document.body.appendChild(script);  
     }
-
-
-    if(!order || order._id !== id || successPay) {
-      dispatch(getOrderDetails(id))
-    } else if(!order.isPaid){ //Si no se ha pagado la orden, no muestra la interfaz de payPal
-        if(!window.paypal){ 
-          addPayPalScript()
-        }else{
-          setSdkReady(true)
-        }
-    }
-  },[dispatch,id,order,successPay])
+  }, [dispatch, id, order, successPay]);
 
  
+  const successPaymentHandler = (paymentResult) => {
+    console.log(paymentResult)
+    dispatch(payOrder(id, paymentResult))
+  }
 
-  return loading ? <Loader/> : error ? <Message variant='danger'>{error}</Message>:<>
-    <h1>Order {order._id}</h1>
-    <Row>
+
+
+
+  return loading ? (
+    <Loader />
+  ) : error ? (
+    <Message variant="danger">{error}</Message>
+  ) : (
+    <>
+      <h1>Order {order._id}</h1>
+      <Row>
         <Col md={8}>
           <ListGroup variant="flush">
             <ListGroup.Item>
               <h2>Shipping</h2>
-              <p><strong>Name: </strong>{order.user.name}</p>
-              <p><strong>Email: </strong><a href={`mailto${order.user.email}`}>{order.user.email}</a></p>
+              <p>
+                <strong>Name: </strong>
+                {order.user.name}
+              </p>
+              <p>
+                <strong>Email: </strong>
+                <a href={`mailto${order.user.email}`}>{order.user.email}</a>
+              </p>
 
               <p>
                 <strong>Address: </strong>
@@ -86,10 +107,12 @@ const OrderScreen = () => {
                 {order.shippingAddress.postalCode}
                 {order.shippingAddress.country}
               </p>
-              {order.idDelivered?(
-                <Message variant='success'>Delivered on {order.idDelivered}</Message>
-              ):(
-                <Message variant='danger'>Not Delivered</Message>
+              {order.idDelivered ? (
+                <Message variant="success">
+                  Delivered on {order.idDelivered}
+                </Message>
+              ) : (
+                <Message variant="danger">Not Delivered</Message>
               )}
             </ListGroup.Item>
 
@@ -99,11 +122,13 @@ const OrderScreen = () => {
                 <strong>Method: </strong>
                 {order.paymentMethod}
               </p>
-              {order.isPaid?(
-                <Message variant='success'>Paid on {order.paidAt}</Message>
-              ):(
-                <Message variant='danger'>Not Paid</Message>
-              )}
+              {order.isPaid ? (
+                <Message variant="success">Paid on {order.paidAt}</Message> 
+              ) : (
+                <Message variant="danger">Not Paid</Message>
+              )
+              }
+              {console.log({order_paidAt:order.paidAt})}
             </ListGroup.Item>
 
             <ListGroup.Item>
@@ -141,7 +166,7 @@ const OrderScreen = () => {
         </Col>
 
         <Col md={4}>
-          <Card style={{height:"100%"}}>
+          <Card style={{ height: '100%' }}>
             <ListGroup variant="flush">
               <ListGroup.Item>
                 <h2>Order Summary</h2>
@@ -173,12 +198,26 @@ const OrderScreen = () => {
                   <Col>Total</Col>
                   <Col>${order.totalPrice}</Col>
                 </Row>
-              </ListGroup.Item>             
+              </ListGroup.Item>
+              {!order.isPaid && (
+                <ListGroup.Item>
+                  {loadingPay && <Loader />}
+                  {!sdkReady ? (
+                    <Loader />
+                  ) : (
+                    <PayPalButton
+                      amount={order.totalPrice}
+                      onSuccess={successPaymentHandler}
+                    />
+                  )}
+                </ListGroup.Item>
+              )}
             </ListGroup>
           </Card>
         </Col>
       </Row>
-  </>
+    </>
+  );
 };
 
 export default OrderScreen;
