805f9ebf9a6a908aaa3a8a73b92f99b4e33e9c94
JoacoMaurtua <joacomaurtua@gmail.com>
2022-02-16 12:07:53 -0500

    payOrder action

--- client/src/actions/orderActions.js
+++ client/src/actions/orderActions.js
@@ -5,6 +5,10 @@ import {
   ORDER_DETAILS_REQUEST,
   ORDER_DETAILS_SUCCESS,
   ORDER_DETAILS_FAIL,
+  ORDER_PAY_REQUEST,
+  ORDER_PAY_SUCCESS,
+  ORDER_PAY_FAIL,
+  ORDER_PAY_RESET,
 } from '../constants/orderConstants';
 import axios from 'axios';
 
@@ -74,6 +78,44 @@ export const getOrderDetails = (id) => async (dispatch, getState) => { //objeto 
   } catch (error) {
     dispatch({
       type: ORDER_DETAILS_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+}
+
+
+export const payOrder = (orderId, paymentResult) => async (dispatch, getState) => { //paymentResult viene de Paypal
+  try {
+    dispatch({
+      type: ORDER_PAY_REQUEST,
+    })
+
+    //Autorizacion del token y su uso para lo que sea
+    const {
+      userLogin: { userInfo },
+    } = getState()
+
+    const config = {
+      headers: {
+        'Content-Type':'application.json',
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    }
+
+    const { data } = await axios.put(`/api/order/${orderId}/pay`,paymentResult, config)
+
+    
+    dispatch({
+      type: ORDER_PAY_SUCCESS,
+      payload: data,
+    })
+   
+  } catch (error) {
+    dispatch({
+      type: ORDER_PAY_FAIL,
       payload:
         error.response && error.response.data.message
           ? error.response.data.message
