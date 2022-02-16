5696fbe3e710083caa5d16bf09c55e8d6960caee
JoacoMaurtua <joacomaurtua@gmail.com>
2022-02-16 11:57:03 -0500

    order pay reducer

--- client/src/store.js
+++ client/src/store.js
@@ -4,7 +4,7 @@ import {composeWithDevTools} from 'redux-devtools-extension'; //metodo para que 
 import {productListReducer, productListDetailsReducer} from './reducers/productReducers';
 import {cartReducer} from './reducers/cartReducers';
 import { userLoginReducer,userRegisterReducer,userDetailsReducer,userUpdateProfileReducer } from './reducers/userReducers';
-import { orderCreateReducer,orderDetailsReducer } from './reducers/orderReducers';
+import { orderCreateReducer,orderDetailsReducer, orderPayReducer } from './reducers/orderReducers';
 //Variable que engloba a los reducers y combina sus propiedades en un solo objeto
 
 //ESTADO GLOBAL:
@@ -18,6 +18,7 @@ const reducer = combineReducers({
   userUpdateProfile:userUpdateProfileReducer,
   orderCreate: orderCreateReducer,
   orderDetails: orderDetailsReducer,
+  orderPay: orderPayReducer,
 
 });
 
