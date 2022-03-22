62393374c0a43189a9b0fee73d5c7e8f7e9bc46d
JoacoMaurtua <joacomaurtua@gmail.com>
2022-03-22 14:56:38 -0500

    recuder order my list

--- client/src/reducers/orderReducers.js
+++ client/src/reducers/orderReducers.js
@@ -9,6 +9,9 @@ import {
   ORDER_PAY_SUCCESS,
   ORDER_PAY_FAIL,
   ORDER_PAY_RESET,
+  ORDER_MYLIST_SUCCESS,
+  ORDER_MYLIST_FAIL,
+  ORDER_MYLIST_REQUEST,
 } from '../constants/orderConstants';
 
 export const orderCreateReducer = (state = {}, action) => {
@@ -84,6 +87,31 @@ export const orderPayReducer = (state = {}, action) => {
 
     case ORDER_PAY_RESET:
       return { };
+    default:
+      return state;
+  }
+};
+
+
+export const orderMyListReducer = (state = {orders:[]}, action) => {
+  switch (action.type) {
+    case ORDER_MYLIST_REQUEST:
+      return {
+        loading: true,
+      };
+
+    case ORDER_MYLIST_SUCCESS:
+      return {
+        loading: false,
+        orders: action.payload,
+      };
+
+    case ORDER_MYLIST_FAIL:
+      return {
+        loading: false,
+        error: action.payload,
+      };
+
     default:
       return state;
   }
--- client/src/store.js
+++ client/src/store.js
@@ -1,10 +1,23 @@
-import {createStore, combineReducers, applyMiddleware} from 'redux'; //crea el store, agrupa todos los reducers, middleware
+import { createStore, combineReducers, applyMiddleware } from 'redux'; //crea el store, agrupa todos los reducers, middleware
 import thunk from 'redux-thunk';
-import {composeWithDevTools} from 'redux-devtools-extension'; //metodo para que las redux dev tools del browser se apliquen al proyecto
-import {productListReducer, productListDetailsReducer} from './reducers/productReducers';
-import {cartReducer} from './reducers/cartReducers';
-import { userLoginReducer,userRegisterReducer,userDetailsReducer,userUpdateProfileReducer } from './reducers/userReducers';
-import { orderCreateReducer,orderDetailsReducer, orderPayReducer } from './reducers/orderReducers';
+import { composeWithDevTools } from 'redux-devtools-extension'; //metodo para que las redux dev tools del browser se apliquen al proyecto
+import {
+  productListReducer,
+  productListDetailsReducer,
+} from './reducers/productReducers';
+import { cartReducer } from './reducers/cartReducers';
+import {
+  userLoginReducer,
+  userRegisterReducer,
+  userDetailsReducer,
+  userUpdateProfileReducer,
+} from './reducers/userReducers';
+import {
+  orderCreateReducer,
+  orderDetailsReducer,
+  orderPayReducer,
+  orderMyListReducer,
+} from './reducers/orderReducers';
 //Variable que engloba a los reducers y combina sus propiedades en un solo objeto
 
 //ESTADO GLOBAL:
@@ -15,45 +28,44 @@ const reducer = combineReducers({
   userLogin: userLoginReducer,
   userRegister: userRegisterReducer,
   userDetails: userDetailsReducer,
-  userUpdateProfile:userUpdateProfileReducer,
+  userUpdateProfile: userUpdateProfileReducer,
   orderCreate: orderCreateReducer,
   orderDetails: orderDetailsReducer,
   orderPay: orderPayReducer,
-
+  orderMyList: orderMyListReducer,
 });
 
 //Almacenar la data del carrito de compras en el local storage
 const cartItemsFromStorage = localStorage.getItem('cartItems')
   ? JSON.parse(localStorage.getItem('cartItems'))
-  : []
+  : [];
 
 //Almacenar la data del usuario en el local storage
 const userInfoFromStorage = localStorage.getItem('userInfo')
-? JSON.parse(localStorage.getItem('userInfo'))
-: null
-
+  ? JSON.parse(localStorage.getItem('userInfo'))
+  : null;
 
 const shippingAddressFromStorage = localStorage.getItem('shippingAddress')
-? JSON.parse(localStorage.getItem('shippingAddress'))
-: {}
+  ? JSON.parse(localStorage.getItem('shippingAddress'))
+  : {};
 
 const initialState = {
-  cart: {cartItems: cartItemsFromStorage, shippingAddress: shippingAddressFromStorage},
-  userLogin: {userInfo: userInfoFromStorage},
-}; 
+  cart: {
+    cartItems: cartItemsFromStorage,
+    shippingAddress: shippingAddressFromStorage,
+  },
+  userLogin: { userInfo: userInfoFromStorage },
+};
 
-const middleware = [thunk] // middleware para hacer llamadas asincronas en el mundo de redux
+const middleware = [thunk]; // middleware para hacer llamadas asincronas en el mundo de redux
 
-const store = createStore(reducer,initialState,composeWithDevTools( //Creamos la unica store de la aplicacion
-  applyMiddleware(...middleware) //accedemos a la funcionalidad del middleware thunk, store es el que gestiona todo
-));
+const store = createStore(
+  reducer,
+  initialState,
+  composeWithDevTools(
+    //Creamos la unica store de la aplicacion
+    applyMiddleware(...middleware) //accedemos a la funcionalidad del middleware thunk, store es el que gestiona todo
+  )
+);
 
 export default store;
-
-
-
-
-
-
-
-
