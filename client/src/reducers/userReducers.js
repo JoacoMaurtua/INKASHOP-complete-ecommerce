54ec47464bfcccd5f531f5b5c906a0730e6b6aeb
JoacoMaurtua <joacomaurtua@gmail.com>
2022-03-28 16:55:26 -0500

    user list reducer

--- client/src/reducers/userReducers.js
+++ client/src/reducers/userReducers.js
@@ -14,6 +14,9 @@ import {
   USER_UPDATE_PROFILE_SUCCESS,
   USER_UPDATE_PROFILE_FAIL,
   USER_UPDATE_PROFILE_RESET,
+  USER_LIST_REQUEST,
+  USER_LIST_SUCCESS,
+  USER_LIST_FAIL,
 
 } from '../constants/userConstants';
 
@@ -93,3 +96,25 @@ export const userUpdateProfileReducer = (state = {}, action) => {
       return state; //retorna el mismo estado en caso no se envie una accion valida
   }
 };
+
+export const userListReducer = (state = {users: []}, action) =>{
+  switch (action.type){
+    case USER_LIST_REQUEST:
+      return {loading: true}
+
+    case USER_LIST_SUCCESS:
+      return {
+        loading: false,
+        users: action.payload
+      }
+
+    case USER_LIST_FAIL:
+      return {
+        loading: false,
+        error: action.payload
+      }
+
+    default: 
+      return state
+  }
+}
