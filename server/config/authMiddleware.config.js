44345edfb04c4c7b5dd00c638b03ca1a7bae140e
JoacoMaurtua <joacomaurtua@gmail.com>
2022-03-28 14:29:32 -0500

    middleware de admin implementado

--- server/config/authMiddleware.config.js
+++ server/config/authMiddleware.config.js
@@ -23,6 +23,17 @@ const protect = asyncHandler(async(req, res, next) => {
     }
   
     next();
-})
+});
 
-module.exports = {protect};
+const admin =(req,res,next) =>{
+    if(req.user && req.user.isAdmin){
+        next();
+    } else{
+        res.status(401)
+        throw new Error('Not authorized as an admin')
+    }
+
+
+}
+
+module.exports = {protect, admin};
--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express();
-const { protect } = require('../config/authMiddleware.config');
+const { protect, admin } = require('../config/authMiddleware.config');
 const {findUsers,authUser, getUserProfile, registerUser, updateUserProfile,findSingleUser} = require('../controllers/user.controllers');
 
 //Rutas:
 
-router.get('/users',findUsers); //Obtiene todos los users
+router.get('/users',protect,admin,findUsers); //Obtiene todos los users
 router.get('/user/:id',protect,findSingleUser) //Obtiene un unico user
 router.post('/register',registerUser);
 router.post('/login',authUser);
