17adec4d7516ea3072448690d11f2747c06ffce8
JoacoMaurtua <joacomaurtua@gmail.com>
2022-03-28 17:02:59 -0500

    user list action

--- client/src/actions/userActions.js
+++ client/src/actions/userActions.js
@@ -13,7 +13,10 @@ import {
   USER_UPDATE_PROFILE_REQUEST,
   USER_UPDATE_PROFILE_SUCCESS,
   USER_UPDATE_PROFILE_FAIL,
-  //USER_UPDATE_PROFILE_RESET
+  //USER_UPDATE_PROFILE_RESET,
+  USER_LIST_REQUEST,
+  USER_LIST_SUCCESS,
+  USER_LIST_FAIL,
 } from '../constants/userConstants';
 
 import {ORDER_MYLIST_RESET} from '../constants/orderConstants';
@@ -202,6 +205,49 @@ export const updateUserProfile = (user) => async (dispatch, getState) => {
     }
     dispatch({
       type: USER_UPDATE_PROFILE_FAIL,
+      payload: message,
+    })
+  }
+}
+
+//RETURN THE USERS LIST
+
+export const listUsers = () => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: USER_LIST_REQUEST,
+    })
+
+    const {
+      userLogin: { userInfo },
+    } = getState()
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    }
+
+    const { data } = await axios.get(`/api/users`, config)
+
+    dispatch({
+      type: USER_LIST_SUCCESS,
+      payload: data,
+    })
+
+    
+    localStorage.setItem('userInfo',JSON.stringify(data))
+    
+  } catch (error) {
+    const message =
+      error.response && error.response.data.message
+        ? error.response.data.message
+        : error.message
+    if (message === 'Not authorized, token failed') {
+      dispatch(logout())
+    }
+    dispatch({
+      type: USER_LIST_FAIL,
       payload: message,
     })
   }
