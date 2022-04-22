2a30c879a8b3e906a8ae02594615860dc3187464
JoacoMaurtua <joacomaurtua@gmail.com>
2022-04-22 11:22:10 -0500

    crear y actualizar producto en controllers

--- server/controllers/product.controllers.js
+++ server/controllers/product.controllers.js
@@ -40,4 +40,60 @@ const deleteProduct = asyncHandler(async (req, res) => {
 })
 
 
-module.exports = {findProduct,findSingleProduct,deleteProduct};
+//Crear un producto -ADMIN-
+const createProduct = asyncHandler(async (req, res) => {
+  const product = new Product({
+    name: 'Sample name',
+    price:0,
+    user: req.user._id,
+    image: '/images/sample.jpg',
+    brand: 'Sample brand',
+    category: 'Sample category',
+    countInStock: 0,
+    numReviews: 0,
+    description: 'Sample description'
+
+  })
+  const createdProduct = await product.save()
+  res.status(201).json(createdProduct)
+});
+
+/* 
+  VERISON ALTERNATIVA:
+  const createProduct = async(req,res) =>{
+  Product.create(req.body)
+      .then(results => res.json({data:results}))
+      .catch(error=>{
+        res.json({error:error, message:'Could not create a task'})
+        res.sendStatus(500);
+      })
+};
+*/
+
+//Actualizar un producto -ADMIN-
+const updateProduct = asyncHandler(async (req, res) => {
+  const {name,price,description,image,brand,category,countInStock} = req.body
+
+  const product = await Product.findById(req.params.id)
+
+  if(product){
+    product.name = name
+    product.price = price
+    product.description = description
+    product.image = image
+    product.brand = brand
+    product.category = category
+    product.countInStock = countInStock
+
+    const updatedProduct = await product.save()
+    res.json(updatedProduct)
+
+  }else{
+    res.status(404)
+    throw new Error('Product not found')
+  }
+
+});
+
+
+module.exports = {findProduct,findSingleProduct,deleteProduct,createProduct,updateProduct};
--- server/data/users.js
+++ server/data/users.js
@@ -4,21 +4,10 @@ const users = [
   {
     name: 'Admin User',
     email: 'admin@example.com',
-    password: bcrypt.hashSync('123456aea',10),
+    password: bcrypt.hashSync('123456',10),
     isAdmin: true
-  },
-
-  {
-    name: 'Joaquin Maurtua',
-    email: 'john@example.com',
-    password: bcrypt.hashSync('123456aea',10)
-  },
-
-  {
-    name: 'Stefano Quiroz',
-    email: 'tefita@example.com',
-    password: bcrypt.hashSync('123456aea',10)
-  },
+  }
+  
 ];
 
 module.exports = users;
