369c7b0919b6cb845a7b3467b7fff39e36d3a6e9
JoacoMaurtua <joacomaurtua@gmail.com>
2022-04-26 14:06:35 -0500

    creando formato del review en la interfaz

--- client/src/views/ProductScreen.js
+++ client/src/views/ProductScreen.js
@@ -1,15 +1,28 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux'; //useDispatch sirve para sirve para llamar una accion //useSelector sirve para seleccionar una parte del estado
 import { Link, useParams, useHistory } from 'react-router-dom';
-import { Row, Col, Image, ListGroup, Card, Button, Form } from 'react-bootstrap';
+import {
+  Row,
+  Col,
+  Image,
+  ListGroup,
+  Card,
+  Button,
+  Form,
+} from 'react-bootstrap';
 import Rating from '../components/Rating';
 import Message from '../components/Message';
 import Loader from '../components/Loader';
-import { listDetailsProduct } from '../actions/productActions';
+import {
+  listDetailsProduct,
+  createProductReview,
+} from '../actions/productActions';
+import { PRODUCT_CREATE_REVIEW_RESET } from '../constants/productConstants';
 
 const Productscreen = () => {
-
-  const [qty,setQty] = useState(1); //cantidad en el stock //quizas sea 1
+  const [qty, setQty] = useState(1); //cantidad en el stock //quizas sea 1
+  const [rating, setRating] = useState(0);
+  const [comment, setComment] = useState('');
 
   const { id } = useParams();
 
@@ -20,26 +33,33 @@ const Productscreen = () => {
   const productDetailList = useSelector((state) => state.productDetails); //extrae datos del estado del store, en este caso la propiedad productList
   const { loading, error, product } = productDetailList;
 
-  useEffect(() => { //llama a la funcion creadora de actions
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
+
+  const productCreateReview = useSelector((state) => state.productCreateReview);
+  const { loading: loadingCreateReview, error: errorCreateReview } =
+    productCreateReview;
+
+  useEffect(() => {
+    //llama a la funcion creadora de actions
     dispatch(listDetailsProduct(id));
   }, [dispatch, id]);
 
-  const addToCart = () =>{
-    history.push(`/cart/${id}?qty=${qty}`)
-  }
-
+  const addToCart = () => {
+    history.push(`/cart/${id}?qty=${qty}`);
+  };
 
   return (
     <>
+      <Link className="btn btn-light my-3" to="/">
+        Go Back
+      </Link>
       {loading ? (
         <Loader />
       ) : error ? (
         <Message variant="danger">{error}</Message>
       ) : (
         <>
-          <Link className='btn btn-light my-3' to="/">
-            Go Back
-          </Link>
           <Row>
             <Col md={6}>
               <Image src={product.image} alt={product.name} fluid />
@@ -86,28 +106,32 @@ const Productscreen = () => {
 
                   {product.countInStock > 0 && (
                     <ListGroup.Item>
-                       <Row>
-                         <Col>Qty</Col>
-                         <Col>
-                          <Form.Select as="select" value={qty} onChange={(e) => setQty(e.target.value)}>
-                            {
-                           
-                              [...Array(product.countInStock).keys()].map(x => (  /* Crear un arreglo con el numero de posiciones que tanga el stock */
-                               
-                                <option key={x+1} value={x+1}>
-                                    {x+1}
-                                  </option>
-                                ))                      
-                            }
+                      <Row>
+                        <Col>Qty</Col>
+                        <Col>
+                          <Form.Select
+                            as="select"
+                            value={qty}
+                            onChange={(e) => setQty(e.target.value)}
+                          >
+                            {[...Array(product.countInStock).keys()].map(
+                              (
+                                x /* Crear un arreglo con el numero de posiciones que tanga el stock */
+                              ) => (
+                                <option key={x + 1} value={x + 1}>
+                                  {x + 1}
+                                </option>
+                              )
+                            )}
                           </Form.Select>
-                         </Col>
-                       </Row>
+                        </Col>
+                      </Row>
                     </ListGroup.Item>
                   )}
 
                   <ListGroup.Item>
                     <Button
-                      onClick = {addToCart}
+                      onClick={addToCart}
                       className="btn-block"
                       type="button"
                       style={{ width: '100%' }}
@@ -118,6 +142,22 @@ const Productscreen = () => {
                   </ListGroup.Item>
                 </ListGroup>
               </Card>
+            </Col>
+          </Row>
+          <Row>
+            <Col md={6}>
+              <h2>Reviews</h2>
+              {product.reviews.length === 0 && <Message>No Reviews</Message>}
+              <ListGroup variant='flush'>
+                {product.reviews.map(review => (
+                  <ListGroup.Item key={review._id}>
+                    <strong>{review.name}</strong>
+                    <Rating value={review.rating}/>
+                    <p>{review.createdAt.substring(0,10)}</p>
+                    <p>{review.comment}</p>
+                  </ListGroup.Item>
+                ))}
+              </ListGroup>
             </Col>
           </Row>
         </>
