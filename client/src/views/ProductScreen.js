6129787c09d29f3c9b2a5e19621ded2ee0ecc2cd
JoacoMaurtua <joacomaurtua@gmail.com>
2022-04-26 14:28:01 -0500

    nuevo avance en la interfaz del review

--- client/src/views/ProductScreen.js
+++ client/src/views/ProductScreen.js
@@ -37,10 +37,14 @@ const Productscreen = () => {
   const { userInfo } = userLogin;
 
   const productCreateReview = useSelector((state) => state.productCreateReview);
-  const { loading: loadingCreateReview, error: errorCreateReview } =
+  const { successCreateReview, error: errorCreateReview } =
     productCreateReview;
 
   useEffect(() => {
+    if(successCreateReview){
+      
+    }
+
     //llama a la funcion creadora de actions
     dispatch(listDetailsProduct(id));
   }, [dispatch, id]);
@@ -48,6 +52,14 @@ const Productscreen = () => {
   const addToCart = () => {
     history.push(`/cart/${id}?qty=${qty}`);
   };
+
+  const submitHandler =(e) => {
+    e.preventDefault();
+    dispatch(createProductReview(id,{
+      rating,
+      comment
+    }))
+  }
 
   return (
     <>
@@ -148,15 +160,57 @@ const Productscreen = () => {
             <Col md={6}>
               <h2>Reviews</h2>
               {product.reviews.length === 0 && <Message>No Reviews</Message>}
-              <ListGroup variant='flush'>
-                {product.reviews.map(review => (
+              <ListGroup variant="flush">
+                {product.reviews.map((review) => (
                   <ListGroup.Item key={review._id}>
                     <strong>{review.name}</strong>
-                    <Rating value={review.rating}/>
-                    <p>{review.createdAt.substring(0,10)}</p>
+                    <Rating value={review.rating} />
+                    <p>{review.createdAt.substring(0, 10)}</p>
                     <p>{review.comment}</p>
                   </ListGroup.Item>
                 ))}
+                <ListGroup.Item>
+                  <h2>Write a Customer Review</h2>
+                  {errorCreateReview && (
+                    <Message variant='danger'>{errorCreateReview}</Message>
+                  )}
+                  {userInfo ? (
+                    <Form onSubmit={submitHandler}>
+                      <Form.Group controlId='rating'>
+                        <Form.Label>Rating</Form.Label>
+                        <Form.Control
+                          as='select'
+                          value={rating}
+                          onChange={(e) => setRating(e.target.value)}
+                        >
+                          <option value=''>Select...</option>
+                          <option value='1'>1 - Poor</option>
+                          <option value='2'>2 - Fair</option>
+                          <option value='3'>3 - Good</option>
+                          <option value='4'>4 - Very Good</option>
+                          <option value='5'>5 - Excellent</option>
+
+                        </Form.Control>
+                      </Form.Group>
+                      <Form.Group controlId='comment'>
+                        <Form.Label>Comment</Form.Label>
+                        <Form.Control
+                          as='textarea'
+                          row='4'
+                          value={comment}
+                          onChange={(e) => setComment(e.target.value)} 
+                        ></Form.Control>
+                      </Form.Group>
+                      <Button type='submit' variant='primary'>
+                          Submit
+                      </Button>
+                    </Form>
+                  ) : (
+                    <Message>
+                      Please <Link to="/login">sign in</Link> to write a review
+                    </Message>
+                  )}
+                </ListGroup.Item>
               </ListGroup>
             </Col>
           </Row>
